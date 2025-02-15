e85de614d163e14eb05cffaed000b5d0047f4abd
santimus <forgitman@gmail.com>
2025-02-15 17:16:30 +0300

    pics

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,5 @@
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-suits = ['Dimonds','Hearts','Clubs','Spades']
+# suits = ['Dimonds','Hearts','Clubs','Spades']
+suits = ["\u{2666}","\u{2665}","\u{2663}","\u{2660}"]
 
 puts "#{values.sample} of #{suits.sample}"
